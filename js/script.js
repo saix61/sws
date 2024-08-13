68b9ec733ac6730f0f0422bf7cdeaf81b0da1c34
Aleksandr Kasianenko <saix61@yandex.ru>
2024-08-13 21:52:42 +0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,31 @@
+jQuery(document).ready(function () {
+    // preload
+    // jQuery(".preloader").hide();
+    setTimeout(function () {
+        jQuery(".preloader").fadeOut(500);
+    }, 1500);
+    // scroll
+    function scroll_fn() {
+        var new_scroll = jQuery(window).scrollTop();
+        // progress-line
+        var prcnt = 100 / (jQuery("html").height() - window.innerHeight) * new_scroll;
+        jQuery(".progress-line").css("width", prcnt + "%");
+    };
+    scroll_fn();
+    jQuery(window).on("resize", scroll_fn);
+    jQuery(window).on("scroll", scroll_fn);
+    // brgr
+    jQuery(document).on('click', '.brgr', () => jQuery('header').toggleClass('_active'));
+    // accordion
+    jQuery(document).on('click', '.item-accordion__head', function () {
+        const item = jQuery(this).closest('.item-accordion');
+        const accordion = jQuery(this).closest('.accordion')
+        const itemList = accordion.find('.item-accordion');
+
+        item.hasClass('_active') ?
+            item.removeClass('_active').find('.item-accordion__body').stop().slideUp()
+            :
+            itemList.removeClass('_active').find('.item-accordion__body').stop().slideUp() &
+            item.addClass('_active').find('.item-accordion__body').stop().slideDown()
+    });
+});
